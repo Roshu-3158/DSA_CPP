b34233cf30e42911fbc3fc32b7b42b32fcfa501f
Roshu-3158 <roshandnikam3158@gmail.com>
2023-09-29 18:12:00 +0530

    subarray_with_given_sum

---
+++ 8.5_b_subarray_with_given_sum.cpp
@@ -0,0 +1,8 @@
+// given an unsorted array A of size N of non-negative integers, find a continuous subarray which adds to a given number S.
+
+// Input :
+// N=5, S=12
+// A[] = {1,2,3,7,5}
+
+// Output: 2 4
+// exaplanation : The sum of elements from 2nd position to 4th position is 12. 

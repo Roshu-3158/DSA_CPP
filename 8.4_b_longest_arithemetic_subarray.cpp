17bd475dda2ce76515283cf75c2551e1f955089c
Roshu-3158 <roshandnikam3158@gmail.com>
2023-09-29 10:59:40 +0530

    longest_arithmetic_subarray

--- 8.4_b_longest_arithemetic_subarray.cpp
+++ 8.4_b_longest_arithemetic_subarray.cpp
@@ -5,3 +5,35 @@
 
 // sarasvati has an array of N non-negative integers. the i-th integer of the array us A. she wants to choose a continguous arithmetic subarray from her array that has the maximum length. please help her to determine the length of the longest contiguous arithemetic subarray.
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    int ans = 2;
+    int pd = a[1]-a[0]; // past difference
+    int j=2;
+    int curr =2;
+
+    while(j<n){
+        if(pd == a[j]-a[j-1]){
+            curr++;
+        }
+        else{
+            pd = a[j]-a[j-1];
+            curr = 2;
+        }
+        ans = max(ans,curr);
+        j++;
+    }
+
+    cout<<ans<<endl;
+}

7a7bbdc99658aa5f4f3af0c262f1c766c6ad4157
Roshu-3158 <roshandnikam3158@gmail.com>
2023-09-29 18:31:22 +0530

    smallest_positive_missing_number

---
+++ 8.5_c_smallest_positive_missing_number.cpp
@@ -0,0 +1,9 @@
+// you are given  an array arr[] of N integers including 0. the task is to find the smallest positive missing number  missing from the array.
+
+// example:
+
+// arr= [0,-9,1,3,-4,5]
+// output: 2
+// explanation : here first positive missing number after a is 2
+
+

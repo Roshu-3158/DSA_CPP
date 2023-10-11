deddbc13d33d50a2887126c5aab291a07c8d9690
Roshu-3158 <roshandnikam3158@gmail.com>
2023-10-11 19:59:24 +0530

    matrix_multiplication

---
+++ 9.2_matrix_multiplication.cpp
@@ -0,0 +1,50 @@
+// problem
+// Given two 2-dimensional arrays of sizes n1*n2 and n2*n3. Your task is to multiplt these matrices and output the multiplied matrix.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1, n2, n3;
+    cin>>n1>>n2>>n3;
+
+    int m1[n1][n2];
+    int m2[n2][n3];
+
+    for(int i=0; i<n1; i++){
+        for(int j=0; j<n2; j++){
+            cin>>m1[i][j]; 
+        }
+    }
+
+    for(int i=0; i<n2; i++){
+        for(int j=0; j<n3; j++){
+            cin>>m2[i][j]; 
+        }
+    }
+
+    int ans[n1][n3];
+
+    for(int i=0; i<n1; i++){
+        for(int j=0; j<n3; j++){
+            ans[i][j]=0;
+        }
+    }
+
+    for(int i=0; i<n1; i++){
+        for(int j=0; j<n3; j++){
+            for(int k=0; k<n2; k++){
+                ans[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+
+    for(int i=0; i<n1; i++){
+        for(int j=0; j<n3; j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}

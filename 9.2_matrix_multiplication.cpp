f2f5d28aeec1b7e9e2052d9556e527abd9c5d3dc
Roshu-3158 <roshandnikam3158@gmail.com>
2023-10-19 17:42:20 +0530

    changes in matrix_multiplication

--- 9.2_matrix_multiplication.cpp
+++ 9.2_matrix_multiplication.cpp
@@ -1,6 +1,6 @@
 // problem
 // Given two 2-dimensional arrays of sizes n1*n2 and n2*n3. Your task is to multiplt these matrices and output the multiplied matrix.
-
+// the first matrix row is always equal to the second row column
 #include<iostream>
 using namespace std;
 
@@ -11,26 +11,31 @@ int main(){
     int m1[n1][n2];
     int m2[n2][n3];
 
+    // input for the first matrix
     for(int i=0; i<n1; i++){
         for(int j=0; j<n2; j++){
             cin>>m1[i][j]; 
         }
     }
 
+    // input for the second matrix
     for(int i=0; i<n2; i++){
         for(int j=0; j<n3; j++){
             cin>>m2[i][j]; 
         }
     }
 
+    // make answer array to store multiplication
     int ans[n1][n3];
 
+    // initialize the ans matrix by 0
     for(int i=0; i<n1; i++){
         for(int j=0; j<n3; j++){
             ans[i][j]=0;
         }
     }
 
+    // performing multiplication of two matrix
     for(int i=0; i<n1; i++){
         for(int j=0; j<n3; j++){
             for(int k=0; k<n2; k++){
@@ -39,6 +44,7 @@ int main(){
         }
     }
 
+    // display the output matrix
     for(int i=0; i<n1; i++){
         for(int j=0; j<n3; j++){
             cout<<ans[i][j]<<" ";
